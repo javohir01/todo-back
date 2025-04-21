6957cd1a9a8237b0e91eea92f7de285b04113c27
javohir <ruzikulovjavokhir005@gmail.com>
2025-04-21 19:21:48 +0500

    fix

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api/auth', require('./routes/authRoutes'));
-app.use('/api/todos', require('./routes/todoRoutes'));
+// app.use('/api/auth', require('./routes/authRoutes'));
+// app.use('/api/todos', require('./routes/todoRoutes'));
+
+const authRoutes = require('./routes/authRoutes');
+const todoRoutes = require('./routes/todoRoutes');
+
+app.use('/api/todos', todoRoutes);
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
