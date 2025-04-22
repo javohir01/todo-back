5c096ab323daa4d3d708b6983e3c32c2b85fed37
javohir <ruzikulovjavokhir005@gmail.com>
2025-04-22 19:24:24 +0500

    anthing

--- server.js
+++ server.js
@@ -7,17 +7,25 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost:5000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
 app.use(cors());
 app.use(express.json());
 
-// app.use('/api/auth', require('./routes/authRoutes'));
-// app.use('/api/todos', require('./routes/todoRoutes'));
-
 const authRoutes = require('./routes/authRoutes');
-const todoRoutes = require('./routes/todoRoutes');
+// const todoRoutes = require('./routes/todoRoutes');
 
-app.use('/api/todos', todoRoutes);
+// app.use('/api/todos', todoRoutes);
 app.use('/api/auth', authRoutes);
 
+app.get('/', (req, res) => {
+  res.send('Hello, world!');
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
